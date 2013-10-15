e5d27d6b0da649cfb68d73e3feda9973949f26c5
SaltwaterC <saltwaterc@gmail.com>
2013-10-15 14:26:06 +0300

    Added some vars for text bolding.

--- lib.bash
+++ lib.bash
@@ -1,11 +1,16 @@
 ###
-## Global module loader
+## Global module loader and variable definitions
 ###
 
 export LIB_BASH_VERSION="0.1-dev"
 
+# boolean stuff
 export TRUE=0
 export FALSE=1
+
+# color stuff
+export BOLD=$(tput bold)
+export NORM=$(tput sgr0)
 
 this_dir="$(dirname $BASH_SOURCE[0])"
 for module in $(ls ${this_dir}/modules.lib.bash/*.sh)
